09aa3eaa624011d431550102ef4176863465d974
Miguel Valdez <mavd22mv@gmail.com>
2020-06-07 14:52:20 -0400

    Ultimos cambios agora si sql_3 nivel 2

--- sqls/nivel2/sql_3.sql
+++ sqls/nivel2/sql_3.sql
@@ -1,15 +1,32 @@
-select viaje.id_tour from
 
-/* Primero tendriamos que ir a la tabla clientes para saber la edad, luego ir a la tabla  cliente_x_viaje_x_tour
-para saber los tours que esos clientes han reservado*/
-cliente join cliente_x_viaje_x_tour on cliente.cedula = cliente_x_viaje_x_tour.cedula
+select tabla1.Tour from 
+    (
+        /* Tours activos de mayores de edad*/
+        select viaje.id_tour as Tour from
 
-/* luego vamos a la tabla viaje para obtener la fecha*/
-join viaje on cliente_x_viaje_x_tour.id_tour = viaje.id_tour and cliente_x_viaje_x_tour.id_viaje = viaje.id_viaje
+        /* Primero tendriamos que ir a la tabla clientes para saber la edad, luego ir a la tabla  cliente_x_viaje_x_tour
+        para saber los tours que esos clientes han reservado*/
+        cliente join cliente_x_viaje_x_tour on cliente.cedula = cliente_x_viaje_x_tour.cedula
 
+        /* luego vamos a la tabla viaje para obtener la fecha*/
+        join viaje on cliente_x_viaje_x_tour.id_tour = viaje.id_tour and cliente_x_viaje_x_tour.id_viaje = viaje.id_viaje
 
-/* Queremos que el cliente sea mayor de edad*/
-where cliente.edad >= 18 and 
+        /* Queremos que el cliente sea mayor de edad*/
+        where cliente.edad >= 18 and 
 
-/* Además el tour debe de estar "activo" */
-viaje.fechaInicio > CURRENT_DATE; 
+        /* Además el tour debe de estar "activo" */
+        viaje.fechaInicio > CURRENT_DATE
+    ) tabla1
+left join
+    (
+        /* Tours activos de mayores de edad*/
+        select viaje.id_tour as Tour from
+        cliente join cliente_x_viaje_x_tour on cliente.cedula = cliente_x_viaje_x_tour.cedula
+        join viaje on cliente_x_viaje_x_tour.id_tour = viaje.id_tour and cliente_x_viaje_x_tour.id_viaje = viaje.id_viaje
+        where cliente.edad < 18 and 
+        viaje.fechaInicio > CURRENT_DATE
+    ) tabla2
+ON tabla1.Tour = tabla2.Tour
+
+/* Tomamos los no comunes de la primera tabla*/
+where tabla2.Tour is null
