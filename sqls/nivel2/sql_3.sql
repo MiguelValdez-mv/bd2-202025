2d3aa05124d778179b9050391e0326f786685d14
Miguel Valdez <mavd22mv@gmail.com>
2020-06-06 21:48:23 -0400

    actulizando sql_3 nivel 2

--- sqls/nivel2/sql_3.sql
+++ sqls/nivel2/sql_3.sql
@@ -1,4 +1,15 @@
-select tour.id from cliente, cliente_x_viaje_x_tour
-cliente.edad >= 18 
-and cliente.cedula = cliente_x_viaje_x_tour.cedula
-and cliente_x_viaje_x_tour.id_tour = tour.id
+select viaje.id_tour from
+
+/* Primero tendriamos que ir a la tabla clientes para saber la edad, luego ir a la tabla  cliente_x_viaje_x_tour
+para saber los tours que esos clientes han reservado*/
+cliente join cliente_x_viaje_x_tour on cliente.cedula = cliente_x_viaje_x_tour.cedula
+
+/* luego vamos a la tabla viaje para obtener la fecha*/
+join viaje on cliente_x_viaje_x_tour.id_tour = viaje.id_tour and cliente_x_viaje_x_tour.id_viaje = viaje.id_viaje
+
+
+/* Queremos que el cliente sea mayor de edad*/
+where cliente.edad >= 18 and 
+
+/* Además el tour debe de estar "activo" */
+viaje.fechaInicio > CURRENT_DATE; 
