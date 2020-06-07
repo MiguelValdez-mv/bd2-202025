7fd5a8c1c92d2b080fc93ed9d98a8f1415572332
Miguel Valdez <mavd22mv@gmail.com>
2020-06-06 20:22:31 -0400

    arreglando sql_2

--- sqls/nivel2/sql_2.sql
+++ sqls/nivel2/sql_2.sql
@@ -1,8 +1,25 @@
-SELECT count(punto_ruta_x_tour.id_tour)  from 
+/* Select distinct para evitar valores repetidos*/
+select distinct punto_ruta_x_tour.id_tour from
+
+/* Obtenemos primeramente los id's de los puntos de ruta para luego pasar a la tabla que asocia los puntos
+de ruta con los tours*/
 punto_ruta join punto_ruta_x_tour on punto_ruta.id = punto_ruta_x_tour.id_pto_ruta
-left join cliente_x_viaje_x_tour on punto_ruta_x_tour.id_tour = cliente_x_viaje_x_tour.id_tour  
-where punto_ruta.ciudad = 'Roma' 
+
+/* Según el resultado obtenido en Join anterior obtenemos los tours que tienen asociado a un cliente*/
+left join cliente_x_viaje_x_tour on punto_ruta_x_tour.id_tour = cliente_x_viaje_x_tour.id_tour
+
+/* Buscamos solamente los tours que pasen por roma*/
+where punto_ruta.ciudad = 'Roma'
+
+/* Decimos is NULL porque no queremos que en este caso los tours NO tengan asociado un cliente*/
 and cliente_x_viaje_x_tour.id_tour is NULL
+
+
+
+
+
+
+
 
 
 
