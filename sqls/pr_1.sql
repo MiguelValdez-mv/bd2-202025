0ad42e25018b8eb455543453098c2f35cdb08da9
Miguel Valdez <mavd22mv@gmail.com>
2020-06-17 19:35:17 -0400

    modificando pr_1

--- sqls/pr_1.sql
+++ sqls/pr_1.sql
@@ -7,14 +7,13 @@ DELIMITER $$
 CREATE PROCEDURE Crear_Viaje(IN idTour INT, IN fechaInicioViaje DATE)
 BEGIN
     declare new_id_viaje int;
-
-    /* Obtenemos el ultimo 1 obtenido en la tabla viaje */
-    set new_id_viaje = ( select id_viaje from viaje
-    order by id_viaje desc
-    limit 1) + 1;
-       
+   
+    /* Tomamos el id más grande de la tabla viaje */
+    set new_id_viaje = (select max(id_viaje) from viaje) + 1;
+      
     /* Insertamos los datos obtenidos por parametros en la tabla Viajes*/
     INSERT INTO viaje (id_viaje,id_tour,fechainicio,fechafin) VALUES (new_id_viaje, idTour, fechaInicioViaje, NULL);
     
-END$$
+END $$
 DELIMITER ;
+
