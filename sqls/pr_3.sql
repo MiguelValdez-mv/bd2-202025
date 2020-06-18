0333f1fbcc334a0633f22b4d21bdf55243f68d61
Miguel Valdez <mavd22mv@gmail.com>
2020-06-18 05:00:40 -0400

    editando pr_3

--- sqls/pr_3.sql
+++ sqls/pr_3.sql
@@ -9,14 +9,13 @@ DELIMITER $$
 CREATE PROCEDURE cliente_frecuente()
 BEGIN
     
-    update cliente inner join cliente_x_viaje_x_tour on cliente.cedula = cliente_x_viaje_x_tour.cedula
-    set es_frecuente = IF(      
-        (
-            select count(*) from cliente, cliente_x_viaje_x_tour
-            where cliente.cedula = cliente_x_viaje_x_tour.cedula
-            and cliente_x_viaje_x_tour.fecha_reserva BETWEEN NOW() - INTERVAL 365 DAY AND NOW()
-        ) > 2, true, false )
-    where cliente.cedula = cliente_x_viaje_x_tour.cedula;
-   
+    update cliente actual
+    set actual.es_frecuente = if( (select count(*) from cliente,cliente_x_viaje_x_tour
+                                where cliente.cedula = cliente_x_viaje_x_tour.cedula
+                                and actual.cedula = cliente.cedula
+                                and cliente_x_viaje_x_tour.fecha_reserva > NOW() - interval 1 year) >=3,
+                                true,
+                                false 
+                            );
 END $$
 DELIMITER ;
